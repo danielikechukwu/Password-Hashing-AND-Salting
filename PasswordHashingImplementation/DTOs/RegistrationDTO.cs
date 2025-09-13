baed39d8f86f706546c97cd4990fd388475f6d35
Daniel ikechukwu <ikechukwudaniel080@gmail.com>
2025-09-13 19:43:15 +0100

    Add project files.

---
+++ PasswordHashingImplementation/DTOs/RegistrationDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordHashingImplementation.DTOs
+{
+    public class RegistrationDTO
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First name can only contain letters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last name can only contain letters.")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters.")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&]).{8,}$",
+            ErrorMessage = "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.")]
+        public string Password { get; set; }
+    }
+}
---
+++ PasswordHashingImplementation/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PasswordHashingImplementation.Models
+{
+    [Index(nameof(Email), Name = "Index_Email", IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string PasswordSalt { get; set; }
+        }
+}
